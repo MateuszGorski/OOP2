7ee6cda9e32fc0418c0183802fab26710d668f44
Kwadratowy <m.gorski@op.pl>
2019-01-13 10:48:56 +0100

    added clear code folder

--- day3-task/src/index.js
+++ day3-task/src/index.js
@@ -1,2 +1,24 @@
 var accordion = document.querySelector('.accordion');
-console.warn(accordion);
+// console.warn(accordion);
+
+accordion.addEventListener('click', function(){
+    accordion.classList.toggle('is-open');
+});
+
+
+var accordion = document.querySelectorAll('.accordion');
+
+for (var i=0; i<accordion.clientHeight; i++){
+    accordion[i].addEventListener('click', function(e){
+
+        for (var n = 0; n<accordion.length; n++){
+            if (e.curentTarget !=accordion[n]){
+                accordion[n].classList.remove('is-open');
+            }
+        }
+
+        this.classList.toggle('is-open');
+    });
+}
+
+
